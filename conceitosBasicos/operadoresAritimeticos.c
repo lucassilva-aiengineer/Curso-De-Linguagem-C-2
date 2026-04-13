98cdb44fa9a7ff9e4d825e90e03dd8cfaa60aa5a
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-04-13 14:01:00 -0300

    Adicionando conceitos

--- conceitosBasicos/operadoresAritimeticos.c
+++ conceitosBasicos/operadoresAritimeticos.c
@@ -1,10 +1,38 @@
 #include <stdio.h> 
 
 // Operadores aritiméricos
-
+// São simbolos matemáticos que indicam algumas operações. 
+// (=, +, -, *, /, %, ++, --)
+// = Operador de atribuição. 
 
 int main(){
 
+    int numeroA = 10; 
+    int numeroB = 20; 
+
+    float numeroC = 5; 
+    float numeroD = 2; 
+
+    // double resultado = numeroA + numeroB;
+    double resultado = numeroA - numeroB;
+    // double resultado = numeroA * numeroB; 
+    // double resultado = numeroC / numeroD; 
+
+    // printf("Resultado: %lf", resultado);
+
+    // Decrementando e incrementando valores. 
+
+    numeroA ++; 
+
+    printf("Resultado: %d\n", numeroA);
+
+    numeroA --; 
+    numeroA --;
+    numeroA --; 
+
+    printf("Resultado: %d\n", numeroA); 
+
+    
 
     return 0; 
 }
