521c425e1427e55da6b43b53bd669bb7dd0eedf1
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2026-05-13 17:31:56 -0300

    Adicionando novas partes

--- projetosPraticos/projeto2/main.c
+++ projetosPraticos/projeto2/main.c
@@ -12,17 +12,36 @@ int main(){
     char adjective3[50] = "";
 
 
-    printf("Indique o substantivo: ");
+
+    printf("Enter an adjective (description): "); 
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0'; // Um carcacter que indica o final da string 
+    
+    // Uma string se trata de uma cadeia de caracteres. 
+
+    printf("Enter a noun (animal or person): ");
     fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0'; // Um carcacter que indica o final da string 
 
-    printf("Indique um adjetivo: ");
-    fgets(adjective1, sizeof(adjective1), stdin); 
+    printf("Enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin); 
+    adjective1[strlen(adjective2) - 1] = '\0'; // Um carcacter que indica o final da string 
+
+    printf("Enter a verb (ending w/ -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0'; // Um carcacter que indica o final da string 
 
 
-    printf("Indique o verbo: ");
-    fgets(verb, sizeof(verb), stdin); 
+    printf("Enter an verb adjective (description): ");
+    fgets(verb, sizeof(verb), stdin);
 
-    // printf("O %s %s esta %s", noun, adjective1, verb);
+
+    // Consluindo 
+    printf("%s\n", noun);
+    printf("%s\n", verb);
+    printf("%s")
+
+
 
 
     return 0;
